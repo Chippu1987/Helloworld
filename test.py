e0e9a14563fd27c44dd9aa97a6b5e00678274f9a
karthikeyan Ayyasamy <karthik@karthikeyans-MacBook-Pro.local>
2023-12-07 13:56:22 -0500

    adding unit testcaases

---
+++ test.py
@@ -0,0 +1,12 @@
+import main
+import unittest
+
+class TestHelloWorld(unittest.TestCase):
+    
+    def test_hello_world(self):
+        func_output=main.hello_world(self)
+        print(func_output)
+        self.assertEqual(func_output,'Hello World - Github Actions!')
+
+if __name__ == '__main__':
+    unittest.main()
