958c15a3e798e9a3e1eca408a5d5e24480f72d26
Chippu1987 <61527563+Chippu1987@users.noreply.github.com>
2023-12-15 10:47:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         func_output=main.hello_world(self)
         print(func_output)
-        self.assertEqual(func_output,'Hello World - Github Actions!')
+        self.assertEqual(func_output,'Hello DevX - Github Actions!')
 
 if __name__ == '__main__':
     unittest.main()
